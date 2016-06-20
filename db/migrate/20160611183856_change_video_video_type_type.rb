9249d66970142d2efd721a6f5c78def6c2086c6e
mzeimet <pib.marvin.zeimet@htw-saarland.de>
2016-06-20 21:33:08 +0200

    changed migrations for heroku v2

--- db/migrate/20160611183856_change_video_video_type_type.rb
+++
@@ -1,8 +0,0 @@
-class ChangeVideoVideoTypeType < ActiveRecord::Migration
-  def self.up
-    change_column :videos, :videoType, :integer
-  end
-  def self.down
-    change_column :videos, :videoType, :string
-  end
-end

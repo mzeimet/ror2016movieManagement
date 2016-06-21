a8f4ebcede3886851a08a48ef10a4c9ad593289d
mzeimet <pib.marvin.zeimet@htw-saarland.de>
2016-06-21 14:00:36 +0200

    fog upload changed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,14 +9,7 @@ CarrierWave.configure do |config|
   }
 
   # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
     config.storage = :fog
-  end
-
 #  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
   config.fog_directory    = ENV['BUCKET_NAME']
